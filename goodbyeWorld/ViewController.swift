09062471b46816465495d02da3a576356a376fe4
Alex Hawkins <alex@tests-MacBook-Pro.local>
2017-02-27 13:01:05 -0500

    Initial Commit

---
+++ goodbyeWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goodbyeWorld
+//
+//  Created by Alex Hawkins on 2/27/17.
+//  Copyright © 2017 Alex Hawkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
